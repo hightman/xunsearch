2ff9dc3593156adc9e57c366483db96a3b1c1075
hightman <hightman@twomice.net>
2011-11-14 11:05:21 +0800

    发布 1.1.0 正式版

--- sdk/php/lib/xs_cmd.inc.php
+++ sdk/php/lib/xs_cmd.inc.php
@@ -1,5 +1,5 @@
 <?php
-/* Automatically generated at 2011/10/25 16:33 */
+/* Automatically generated at 2011/11/14 11:02 */
 define('CMD_NONE',	0);
 define('CMD_DEFAULT',	CMD_NONE);
 define('CMD_PROTOCOL',	20110707);
@@ -143,6 +143,6 @@ define('PACKAGE_BUGREPORT',	"http://www.xunsearch.com/bugs");
 define('PACKAGE_NAME',	"xunsearch");
 define('PACKAGE_TARNAME',	"xunsearch");
 define('PACKAGE_URL',	"");
-define('PACKAGE_VERSION',	"1.1.0-dev");
+define('PACKAGE_VERSION',	"1.1.0");
 /* end the cmd defination */
 
