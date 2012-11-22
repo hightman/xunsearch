01a5c589c214d81a5e5733d1034dc0f41a60c206
hightman <hightman@twomice.net>
2012-11-22 15:33:06 +0800

    在文档首页加入 QQ 群号码

--- sdk/php/lib/xs_cmd.inc.php
+++ sdk/php/lib/xs_cmd.inc.php
@@ -1,5 +1,5 @@
 <?php
-/* Automatically generated at 2012/11/21 14:41 */
+/* Automatically generated at 2012/11/22 15:32 */
 define('CMD_NONE',	0);
 define('CMD_DEFAULT',	CMD_NONE);
 define('CMD_PROTOCOL',	20110707);
@@ -159,6 +159,6 @@ define('PACKAGE_BUGREPORT',	"http://www.xunsearch.com/bugs");
 define('PACKAGE_NAME',	"xunsearch");
 define('PACKAGE_TARNAME',	"xunsearch");
 define('PACKAGE_URL',	"");
-define('PACKAGE_VERSION',	"1.3.3");
+define('PACKAGE_VERSION',	"1.3.4-dev");
 /* end the cmd defination */
 
