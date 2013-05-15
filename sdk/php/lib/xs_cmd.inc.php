104c43afefd24c1d8ace7a47dec81548a543e78e
hightman <hightman@twomice.net>
2013-05-15 18:20:04 +0800

    删除 configure.ac 中一些多余的空格符号

--- sdk/php/lib/xs_cmd.inc.php
+++ sdk/php/lib/xs_cmd.inc.php
@@ -1,5 +1,5 @@
 <?php
-/* Automatically generated at 2013/05/15 12:27 */
+/* Automatically generated at 2013/05/15 16:09 */
 define('CMD_NONE',	0);
 define('CMD_DEFAULT',	CMD_NONE);
 define('CMD_PROTOCOL',	20110707);
