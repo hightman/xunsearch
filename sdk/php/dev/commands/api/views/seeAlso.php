11099366ed92fc95abc92288691f6ebb35537095
hightman <hightman@twomice.net>
2014-12-16 17:15:22 +0800

    移除 sdk/php 目录，即将改用 git subtree

--- sdk/php/dev/commands/api/views/seeAlso.php
+++
@@ -1,9 +0,0 @@
-<?php if(empty($object->see)) return; ?>
-<div class="SeeAlso">
-<h4>参见</h4>
-<ul>
-<?php foreach($object->see as $url): ?>
-	<li><?php echo $url; ?></li>
-<?php endforeach; ?>
-</ul>
-</div>
