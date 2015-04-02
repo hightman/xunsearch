f2861c813dc7a024e11367d11b8aa990a630dfaf
hightman <hightman@twomice.net>
2015-04-02 21:55:10 +0800

    在 SDK 下载文档添加 composer 链接

--- sdk/php/lib/xs_cmd.inc.php
+++ sdk/php/lib/xs_cmd.inc.php
@@ -1,5 +1,5 @@
 <?php
-/* Automatically generated at 2015/04/02 21:20 */
+/* Automatically generated at 2015/04/02 21:51 */
 define('XS_CMD_NONE',	0);
 define('XS_CMD_DEFAULT',	XS_CMD_NONE);
 define('XS_CMD_PROTOCOL',	20110707);
@@ -170,5 +170,5 @@ define('XS_PACKAGE_BUGREPORT',	"http://www.xunsearch.com/bugs");
 define('XS_PACKAGE_NAME',	"xunsearch");
 define('XS_PACKAGE_TARNAME',	"xunsearch");
 define('XS_PACKAGE_URL',	"");
-define('XS_PACKAGE_VERSION',	"1.4.9");
+define('XS_PACKAGE_VERSION',	"1.4.10-dev");
 /* end the cmd defination */
