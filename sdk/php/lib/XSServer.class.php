b1cc9619ae732a430a8b01a47b52bef179b5ebae
Node <8974108+qwenode@users.noreply.github.com>
2022-08-01 14:03:40 +0800

    php-fpm环境下,max_execution_time 的值可能是 max_execution_time=30s, 会导致$timeout > 0 出错

--- sdk/php/lib/XSServer.class.php
+++ sdk/php/lib/XSServer.class.php
@@ -478,7 +478,7 @@ class XSServer extends XSComponent
 		}
 
 		// set socket options
-		$timeout = (int)trim(ini_get('max_execution_time'),'s');
+		$timeout = intval(ini_get('max_execution_time'));
 		$timeout = $timeout > 0 ? ($timeout - 1) : 30;
 		stream_set_blocking($sock, true);
 		stream_set_timeout($sock, $timeout);
