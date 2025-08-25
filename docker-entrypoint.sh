8ca60c7274a4ffbd2608de8ec1613dfb0a890a96
hightman <hightman@twomice.net>
2025-08-25 08:46:21 +0800

    Improve Dockerfile.alpine to make the image smaller.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,35 @@
-#!/bin/sh -e
+#!/bin/sh
 # timezone
 zone_info=/usr/share/zoneinfo/$TZ
 if [ -n "$TZ" ] && [ -e $zone_info ]; then
   ln -sf $zone_info /etc/localtime && echo $TZ > /etc/timezone
 fi
+
 # servers
-rm -f tmp/pid.*
+sig_stop() {
+  echo "ENTRY-POINT: stop signal received."
+  bin/xs-indexd -k stop
+  bin/xs-searchd -k stop
+}
+trap sig_stop SIGTERM SIGINT SIGQUIT
+
+echo "ENTRY-POINT: starting servers..."
 echo -n > tmp/docker.log
 bin/xs-indexd -l tmp/docker.log -k start
 bin/xs-searchd -l tmp/docker.log -k start
-# display log
-tail -f tmp/docker.log
+tail -f tmp/docker.log &
+
+wait
+
+pid1=$(cat tmp/pid.8383)
+pid2=$(cat tmp/pid.8384)
+echo -n "ENTRY-POINT: waiting for servers to exit."
+while true; do
+  if kill -0 $pid1 2>/dev/null || kill -0 $pid2 2>/dev/null; then
+    echo -n "."
+    sleep 1
+  else
+    break
+  fi
+done
+echo
